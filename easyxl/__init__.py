10749307a68eb549c9318eedeb3d52f141967eb1
Richard Carter <goobysc@gmail.com>
2024-03-22 13:31:48 -0400

    add main objects to root of module

--- easyxl/__init__.py
+++ easyxl/__init__.py
@@ -1,1 +1,10 @@
-from .editor import ExcelEditor
+from .base import (
+    ExcelRange,
+    ExcelRangeWritable,
+    ExcelTable,
+    ExcelTableWritable,
+    NewExcelTable,
+)
+from .editors import WorkbookEditor
+from .reader import WorkbookReader
+from .creator import WorkbookCreator
